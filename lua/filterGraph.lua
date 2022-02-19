1ef62c8d7c22530aeaed2e4f8da922e5bb452f3f
Trent Johnson <trentj@netflixcontractors.com>
2022-02-19 15:27:32 -0800

    first try at graphing pitch env

---
+++ lua/filterGraph.lua
@@ -0,0 +1,8 @@
+--
+-- Called when a modulator value changes
+-- @mod   http://ctrlr.org/api/class_ctrlr_modulator.html
+-- @value    new numeric value of the modulator
+--
+filterGraph = function(--[[ CtrlrModulator --]] mod, --[[ number --]] value, --[[ number --]] source)
+	console("graph2")
+end
