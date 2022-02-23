1ac571053145f69ee024319404418c7ea9ccf9b9
Trent Johnson <trentj@efn.org>
2022-02-22 19:16:39 -0800

    flesh out pcm/wav switching and add some sliders/buttons

--- lua/partialSelect.lua
+++ lua/partialSelect.lua
@@ -10,5 +10,10 @@ function partialSelect(mod, value, source)
 
     panel:getComponent("tabs-partials"):setProperty("uiTabsCurrentTab", partial-1, false)
     updateLCD("Tone Edit", "Partial ".. partial)
+
+    -- don't allow the currently pressed button to get in an off state
+    if get("btn-p".. partial) == 0 then
+        set("btn-p".. partial, 1)
+    end
 end
 
