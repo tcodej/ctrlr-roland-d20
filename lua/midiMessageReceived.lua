88fbbfd2c1d3fb3d6d9a1c1b70c3b08aeb1e6d57
Trent Johnson <trentj@efn.org>
2022-03-01 23:12:40 -0800

    a semi-successful attempt at sending a reset tone to the device

--- lua/midiMessageReceived.lua
+++ lua/midiMessageReceived.lua
@@ -1,9 +1,9 @@
 local mm
-local count = 1
+local data
 
 function midiMessageReceived(midiMessage)
     mm = midiMessage
-    --count = count + 1
+    data = mm:getData()
 
 	if panel:getRestoreState() == true or panel:getProgramState() == true then
 		return
@@ -18,26 +18,17 @@ function midiMessageReceived(midiMessage)
         activity(ACT_IN)
     end
 
-	size = mm:getSize()
-    --console(tostring(size))
 
+
+
+--[[
     updateLCD(
         getByteHex(0) .." ".. getByteHex(1) .." ".. getByteHex(2) .." ".. getByteHex(3) .." ".. getByteHex(4),
         getByteHex(5) .." ".. getByteHex(6) .." ".. getByteHex(7) .." ".. getByteHex(8) .." ".. getByteHex(9)
     )
+--]]
 
---]]
-    tempTones = {
-        "04 00 00",
-        "04 01 76",
-        "04 03 6c",
-        "04 05 62",
-        "04 07 58",
-        "04 09 4E",
-        "04 0b 44",
-        "04 0d 3a"
-    }
---]]
+	local size = mm:getSize()
 
     if size == 256 then
         activity(ACT_SYS)
@@ -45,8 +36,8 @@ function midiMessageReceived(midiMessage)
             setTone()
         end
 
-        data = mm:getLuaData() -- or getLuaData():getRange(0,24) etc.
-        file = File("d:\\sysexdump\\tone".. count ..".syx")
+        console(data:toHexString(1))
+        file = File("d:\\sysexdump\\tone1.syx")
         file:replaceWithData(data)
     end
 
@@ -56,16 +47,21 @@ end
 
 -- return a hex byte
 function getByteHex(num)
-    if mm == nil then return end
+    if data == nil then return end
 
-    return numToHex(mm:getData():getByte(num))
+    return numToHex(data:getByte(num))
 end
 
 
 
 -- return a decimal byte
 function getByteNum(num)
-    if mm == nil then return end
+    if data == nil then return end
 
-    return mm:getData():getByte(num)
+    return data:getByte(num)
+end
+
+
+function setData(block)
+    data = block
 end
