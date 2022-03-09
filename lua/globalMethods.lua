539f04edb76348fcc5e02aab45136be695ed0078
Trent Johnson <trentj@efn.org>
2022-03-08 20:17:58 -0800

    code cleanup

--- lua/globalMethods.lua
+++ lua/globalMethods.lua
@@ -36,11 +36,17 @@ ASCII = " !\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abc
 P_EDIT = {true, false, false, false}
 OFF_ON = {" OFF"," ON "}
 WAVE_SHAPE = {"SQU ","SAW ","SQU ","SAW "}
+
 BIAS_PT = {"<A1 ","<A#1","<B1 ","<C1 ","<C#1","<D1 ","<D#1","<E1 ","<F1 ","<F#1","<G1 ","<G#1","<A2 ","<A#2","<B2 ","<C2 ","<C#2","<D2 ","<D#2","<E2 ","<F2 ","<F#2","<G2 ","<G#2","<A3 ","<A#3","<B3 ","<C3 ","<C#3","<D3 ","<D#3","<E3 ","<F3 ","<F#3","<G3 ","<G#3","<A4 ","<A#4","<B4 ","<C4 ","<C#4","<D4 ","<D#4","<E4 ","<F4 ","<F#4","<G4 ","<G#4","<A5 ","<A#5","<B5 ","<C5 ","<C#5","<D5 ","<D#5","<E5 ","<F5 ","<F#5","<G5 ","<G#5","<A6 ","<A#6","<B6 ","<C7 ",">A1 ",">A#1",">B1 ",">C1 ",">C#1",">D1 ",">D#1",">E1 ",">F1 ",">F#1",">G1 ",">G#1",">A2 ",">A#2",">B2 ",">C2 ",">C#2",">D2 ",">D#2",">E2 ",">F2 ",">F#2",">G2 ",">G#2",">A3 ",">A#3",">B3 ",">C3 ",">C#3",">D3 ",">D#3",">E3 ",">F3 ",">F#3",">G3 ",">G#3",">A4 ",">A#4",">B4 ",">C4 ",">C#4",">D4 ",">D#4",">E4 ",">F4 ",">F#4",">G4 ",">G#4",">A5 ",">A#5",">B5 ",">C5 ",">C#5",">D5 ",">D#5",">E5 ",">F5 ",">F#5",">G5 ",">G#5",">A6 ",">A#6",">B6 ",">C7 "}
 BIAS_LVL_TVA = {"-12","-11","-10","-09","-08","-07","-06","-05","-04","-03","-02","-01"," 00"}
 BIAS_LVL_TVF = {"-07","-06","-05","-04","-03","-02","-01"," 00","+01","+02","+03","+04","+05","+06","+07"}
+
 KEY_FOL = {"-1  ","-1/2","-1/4"," 0  "," 1/8"," 1/4"," 3/8"," 1/2"," 5/8"," 3/4"," 7/8"," 1  "," 5/4"," 3/2"," 2  "}
-KEY_FOL_PITCH = {} -- values set below
+KEY_FOL_PITCH = KEY_FOL
+table.insert(KEY_FOL_PITCH, " s1 ")
+table.insert(KEY_FOL_PITCH, " s2 ")
+
+
 PITCH_COARSE = {" C1 "," C#1"," D1 "," D#1"," E1 "," F1 "," F#1"," G1 "," G#1"," A1 "," A#1"," B1 "," C2 "," C#2"," D2 "," D#2"," E2 "," F2 "," F#2"," G2 "," G#2"," A2 "," A#2"," B2 "," C3 "," C#3"," D3 "," D#3"," E3 "," F3 "," F#3"," G3 "," G#3"," A3 "," A#3"," B3 "," C4 "," C#4"," D4 "," D#4"," E4 "," F4 "," F#4"," G4 "," G#4"," A4 "," A#4"," B4 "," C5 "," C#5"," D5 "," D#5"," E5 "," F5 "," F#5"," G5 "," G#5"," A5 "," A#5"," B5 "," C6 "," C#6"," D6 "," D#6"," E6 "," F6 "," F#6"," G6 "," G#6"," A6 "," A#6"," B6 "," C7 "," C#7"," D7 "," D#7"," E7 "," F7 "," F#7"," G7 "," G#7"," A7 "," A#7"," B7 "," C8 "," C#8"," D8 "," D#8"," E8 "," F8 "," F#8"," G8 "," G#8"," C9 "}
 
 LEVELS = {}
@@ -185,8 +191,6 @@ function checkSum(msg)
         total = total + hexToNum(hex)
     end
 
-    --total = hexToNum(pieces[1]) + hexToNum(pieces[2]) + hexToNum(pieces[3]) + hexToNum(pieces[4])
-
     cs = (128 - total) % 128
 
     --console(msg .." checkSum: ".. numToHex(cs))
@@ -270,16 +274,6 @@ function activity(num)
         -- sysex in or out blink blue
         activityImg:setPropertyString("uiImageResource", "led-blue")
     end
-end
-
-
-
-function tableConcat(t1, t2)
-   for i=1,#t2 do
-      t1[#t1+1] = t2[i]
-   end
-
-   return t1
 end
 
 
@@ -380,9 +374,3 @@ function toggleLayerVisible(name, bool)
         layer:setPropertyInt("uiPanelCanvasLayerVisibility", int)
     end
 end
-
-
-
-
--- down here so tableConcat() is available
-KEY_FOL_PITCH = tableConcat(KEY_FOL, {" s1 "," s2 "})
