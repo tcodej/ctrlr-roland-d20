caf63058b3329d137c811b8346734a1243aab1c6
Trent Johnson <trentj@efn.org>
2022-03-17 11:45:12 -0700

    lfo1 updates

--- lua/lfo1.lua
+++ lua/lfo1.lua
@@ -4,17 +4,23 @@
 
 local lfoNum = 1
 
+local SHAPES = {"Triangle","Square","Sawtooth","Random"}
+
+local MODS = {
+    { name = "mod-timbre-keyshft", min = 0, max = 48 },
+    { name = "tvf-cutoff-p1", min = 30, max = 100 },
+    { name = "tvf-res-p1", min = 15, max = 30 },
+    { name = "form-pw-p1", min = 0, max = 100 }
+}
+
+local dest = MODS[1].name
+local min = MODS[1].min
+local max = MODS[1].max
 local rate = 30
-local min = 0
-local max = 48
-local val = 0
+local val = min
 local step = 1
 local dir = 1
-local dest = "mod-timbre-keyshft"
 local shape = "Triangle"
-
-local SHAPES = {"Triangle","Square","Sawtooth","Random"}
-local MODS = {"mod-timbre-keyshft","tvf-cutoff-p1","tvf-res-p1","form-pw-p1"}
 
 function lfo1(mod, value)
 
@@ -52,7 +58,10 @@ function lfo1(mod, value)
         line2 = shape
 
     elseif string.find(name, "mod") then
-        dest = MODS[value+1]
+        dest = MODS[value+1].name
+        min = MODS[value+1].min
+        max = MODS[value+1].max
+
         line1 = "LFO1 Shape"
         line2 = dest
 
@@ -90,7 +99,6 @@ function lfo1Loop()
 
     else
 
-
         if val >= max then
             dir = -1
         end
@@ -101,8 +109,6 @@ function lfo1Loop()
 
         val = val + (step * dir)
     end
-
-    console(dest)
 
     set(dest, val)
 end
